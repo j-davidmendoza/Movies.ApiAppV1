44e20de10b3416bc1bbe59098f4676a5253b065f
David Mendoza <mendoza.d.jesus@gmail.com>
2025-03-20 15:54:15 -0700

    Defining the movie api contracts

---
+++ Movies.Contracts/Responses/MovieResponses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Responses
+{
+    internal class MovieResponses
+    {
+        public required Guid Id { get; init; }
+        public required string Tittle { get; init; }
+        public required int YearOfRelease { get; init; }
+        public required IEnumerable<string> Genres { get; init; } = Enumerable.Empty<string>();
+    }
+}
---
+++ Movies.Contracts/Responses/MoviesResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Contracts.Responses
+{
+    internal class MoviesResponse
+    {
+        public required IEnumerable<MovieResponses> Item {  get; init; } = Enumerable.Empty<MovieResponses>();
+    }
+}
