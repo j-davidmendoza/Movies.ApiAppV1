6dc5c7c996643ce8bc7f3222c50825afa333985f
David Mendoza <mendoza.d.jesus@gmail.com>
2025-03-25 22:47:03 -0700

    implemented rating a movie

--- Movies.Application/Repositories/RatingRepository.cs
+++ Movies.Application/Repositories/RatingRepository.cs
@@ -42,4 +42,17 @@ public class RatingRepository : IRatingRepository
             where movieid = @movieId
             """, new { movieId, userId }, cancellationToken: token));
     }
+
+    public async Task<bool> RateMovieAsync(Guid movieId, int rating, Guid userId, CancellationToken token)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
+        var result = await connection.ExecuteAsync(new CommandDefinition("""
+            insert into ratings(userid, movieid, rating) 
+            values (@userId, @movieId, @rating)
+            on conflict (userid, movieid) do update 
+                set rating = @rating
+            """, new { userId, movieId, rating }, cancellationToken: token));
+
+        return result > 0;
+    }
 }
